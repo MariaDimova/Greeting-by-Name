dea42de2533b59a8204f8cca67a7d2cdc0c47440
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-17 01:17:44 +0200

    Add files via upload

---
+++ 6.Съединяване на текст и числа.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string age = Console.ReadLine();
+        string town = Console.ReadLine();
+
+        Console.WriteLine("You are" + ' ' + firstName + ' ' + lastName + ',' + ' ' + "a" + ' ' + age  + '-' + "years old person from" + ' '
+          + town + '.') ;
+    }
+}
